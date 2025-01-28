cc569b943ae48b4bef1d81c6696db1cf7f3496f2
BigShark <gaharax.gaharax@gmail.com>
2025-01-28 13:58:44 +0700

    add server setting

--- server.js
+++ server.js
@@ -63,3 +63,26 @@ app.post("/delete", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
+
+const jsonServer = require("json-server");
+const cors = require("cors");
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+// เพิ่ม middleware สำหรับ CORS
+server.use(
+  cors({
+    origin: "*", // หรือกำหนด origin เป็น URL ของ Vercel เช่น 'https://your-vercel-domain.vercel.app'
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
+
+server.use(middlewares);
+server.use(router);
+
+const PORT2 = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT2}`);
+});
