071b36b5c2b1e17d78ca82e74b2f126d5134c2a6
BigShark <gaharax.gaharax@gmail.com>
2025-01-30 12:01:23 +0700

    Fix db

--- src/db.js
+++ src/db.js
@@ -1,9 +1,7 @@
 import Dexie from "dexie";
 
-// ✅ สร้างฐานข้อมูล
 const db = new Dexie("MyDatabase");
 
-// ✅ สร้าง Table (object store)
 db.version(1).stores({
   images: "id,data",
   project: "id,image,name,date,rgb,filter",
